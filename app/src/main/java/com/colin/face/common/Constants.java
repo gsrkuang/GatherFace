d5bcf63bcdfd410ebbbe0e3a05fe150e7004c31a
gsrkuang <gsrkuang@163.com>
2021-03-23 21:04:07 +0800

    GatherFace人脸信采集系统

---
+++ app/src/main/java/com/colin/face/common/Constants.java
@@ -0,0 +1,17 @@
+package com.colin.face.common;
+
+public class Constants {
+
+    public static final String APP_ID = "A8zUZD9TYCa8oQBRsD5VuQJs24h5CpTpmRY343P6G98i";
+    public static final String SDK_KEY = "34H6w99D8aWR9JFGNNEagxpmbLnF7QPZLaJJm1cMXGTr";
+
+    /**
+     * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int HORIZONTAL_OFFSET = 0;
+    /**
+     * IR预览数据相对于RGB预览数据的纵向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int VERTICAL_OFFSET = 0;
+}
+
---
+++ app/src/main/java/com/colin/face/util/face/RequestLivenessStatus.java
@@ -0,0 +1,5 @@
+package com.colin.face.util.face;
+
+public class RequestLivenessStatus {
+    public static final int ANALYZING = 10;
+}
